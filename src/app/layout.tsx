4d12d6e82a85c39dc521a728e0c38ceec0ebed1c
berry1001 <sherif.luca.ilyas@gmail.com>
2025-11-16 18:17:51 +0200

    add router-messenger.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,51 @@
 import type { Metadata } from "next";
-import { Inter, Instrument_Serif } from "next/font/google";
-import "./globals.css";
-import VisualEditsManager from "../visual-edits/VisualEditsManager";
+  import { Inter, Instrument_Serif } from "next/font/google";
+  import "./globals.css";
+  import VisualEditsManager from "../visual-edits/
+  VisualEditsManager";
+  import Script from "next/script";
 
-const interSans = Inter({
+  const interSans = Inter({
   variable: "--font-inter-sans",
   subsets: ["latin"],
-});
+  });
 
-const instrumentSerif = Instrument_Serif({
+  const instrumentSerif = Instrument_Serif({
   variable: "--font-instrument-serif",
   subsets: ["latin"],
   weight: "400",
   style: "italic",
-});
+  });
 
-export const metadata: Metadata = {
+  export const metadata: Metadata = {
   title: "Built by Pastel",
   description: "",
-};
+  };
 
-export default function RootLayout({
+  export default function RootLayout({
   children,
-}: Readonly<{
+  }: Readonly<{
   children: React.ReactNode;
-}>) {
+  }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${interSans.variable} ${instrumentSerif.variable}
-  antialiased`}
-      >
-        {children}
-        <VisualEditsManager />
-      </body>
-    </html>
+  <html lang="en">
+  <body
+  className={${interSans.variable} ${instrumentSerif.variable}
+  antialiased}
+  >
+  <Script
+  src="https://lgnyaweewkitdrnkextq.supabase.co/storage/v1/object/
+  public/Public/route-messenger.js"
+  strategy="afterInteractive"
+  data-target-origin="*"
+  data-message-type="ROUTE_CHANGE"
+  data-include-search-params="true"
+  data-only-in-iframe="true"
+  data-debug="true"
+          />
+  {children}
+  <VisualEditsManager />
+  </body>
+  </html>
   );
-}
+  }
